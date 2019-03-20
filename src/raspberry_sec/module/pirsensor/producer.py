b92a5c59507a1d55062b9540f3c506a50b042801
nagybalint <nagybalint000@gmail.com>
2019-03-20 20:08:09 +0100

    Add HW and floating input checking to the PIR sensor producer

--- src/raspberry_sec/module/pirsensor/producer.py
+++ src/raspberry_sec/module/pirsensor/producer.py
@@ -1,9 +1,11 @@
 import logging
 from raspberry_sec.interface.producer import Producer, ProducerDataManager, ProducerDataProxy, Type
 from raspberry_sec.system.util import ProcessContext
+from raspberry_sec.system.hw_util import is_gpio_floating, check_platform
+
+from importlib import import_module
 
 import time
-import RPi.GPIO as GPIO
 
 class PirsensorProducerDataProxy(ProducerDataProxy):
     pass
@@ -15,28 +17,49 @@ class PirsensorProducer(Producer):
     def __init__(self, parameters: dict):
         super().__init__(parameters)
 
+        # Check if the software is running on a raspberry pi
+        if not check_platform():
+            raise OSError("Not running on a Raspberry Pi. The libraries handling the PIR sensor can only be used on a Raspberry Pi.")
+        self.gpio = import_module('RPi.GPIO')
+
     def register_shared_data_proxy(self):
         ProducerDataManager.register('PirsensorProducerDataProxy', PirsensorProducerDataProxy)
 
     def create_shared_data_proxy(self, manager: ProducerDataManager):
         return manager.PirsensorProducerDataProxy()
     
+    def __test_hw(self):
+
+        # Check if the PIR sensor is properly connected according to the configuration
+        if is_gpio_floating(self.parameters["GPIO_PIR"]):
+            raise IOError("The configured pin of the PIR motion sensor is floating")
+
     def __setup_hw(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.parameters['GPIO_PIR'],GPIO.IN)
+        self.__test_hw()
+        # Set pulldown for GPIO pin
+        self.gpio.setmode(GPIO.BCM)
+        self.gpio.setup(self.parameters['GPIO_PIR'], self.gpio.IN, pull_up_down=self.gpio.PUD_DOWN)
 
     def __teardown_hw(self):
-        GPIO.cleanup()
+        self.gpio.cleanup()
 
     def __capture_data(self):
-        return GPIO.input(self.parameters['GPIO_PIR'])
+        return self.gpio.input(self.parameters['GPIO_PIR'])
 
     def run(self, context: ProcessContext):
         try:
-            PirsensorProducer.LOGGER.info('Producer started')
+            PirsensorProducer.LOGGER.debug('Producer started')
+            
+            # Set initial data as None
+            data_proxy.set_data(None)
+            
+            # Set up the HW for handling the motion sensor
             self.__setup_hw()
+
+            # Get data proxy
             data_proxy = context.get_prop('shared_data_proxy')
 
+            # Read motion status
             while not context.stop_event.is_set():
                 data = self.__capture_data()
                 if data is not None:
@@ -44,8 +67,9 @@ class PirsensorProducer(Producer):
 
                 time.sleep(self.parameters['wait_interval'])
         finally:
+            # Tear down HW accessing the motion sensor
             self.__teardown_hw()
-            PirsensorProducer.LOGGER.info('Producer teardown')
+            PirsensorProducer.LOGGER.debug('Producer teardown')
     
     def get_data(self, data_proxy: ProducerDataProxy):
         PirsensorProducer.LOGGER.debug('Producer called')
---
+++ src/raspberry_sec/system/hw_util.py
@@ -0,0 +1,32 @@
+from uuid import getnode
+
+def check_platform() -> bool:
+	"""
+	Check if the HW platform the SW is running on is a Raspberry Pi
+	:return True if the HW plaform is issued by the Raspberry Pi Foundation, False otherwise
+	"""
+	MAC_OUI_RASPBERRY_FOUNDATION = ['b827eb', 'dca632']
+	
+	mac_addr = getnode()
+	mac_addr_oui = format(mac_addr, '02x')[0:6]
+
+	return mac_addr_oui in MAC_OUI_RASPBERRY_FOUNDATION
+
+def is_gpio_floating(pin: int) -> bool:
+    import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
+    
+    GPIO.setup(pin,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    state1 = GPIO.input(pin)
+
+    GPIO.setup(pin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    state2 = GPIO.input(pin)
+
+    GPIO.setup(pin,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    state3 = GPIO.input(pin)
+
+    if state1 == GPIO.LOW and state2 == GPIO.HIGH and state3 == GPIO.LOW:
+        return True
+    else:
+        return False
+
