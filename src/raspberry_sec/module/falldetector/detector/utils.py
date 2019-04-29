0fb5b6655aebd414bfba85f22d08d0d89679062f
nagybalint <nagybalint000@gmail.com>
2019-04-29 16:00:16 +0200

    Add timestamps and titles to StatePlotter

--- src/raspberry_sec/module/falldetector/detector/utils.py
+++ src/raspberry_sec/module/falldetector/detector/utils.py
@@ -11,18 +11,38 @@ class StatePlotter:
     """
 
     def __init__(self):
-        self.fig = plt.figure()
+        """
+        Constructor
+        """
+        self.fig = plt.figure(figsize=(12, 8))
         self.x = self.fig.add_subplot(2,2,1)
+        self.x.set_title('x [px]')
         self.y = self.fig.add_subplot(2,2,2)
+        self.y.set_title('y [px]')
         self.h2w = self.fig.add_subplot(2,2,3)
+        self.h2w.set_title('height/width')
         self.angle = self.fig.add_subplot(2,2,4)
+        self.angle.set_title('angle [rad]')
 
-    def plot_states(self, states: list, color = 'blue'):
-        self.x.plot([state.x for state in states], color=color, linewidth=1)
-        self.y.plot([state.y for state in states], color=color, linewidth=1)
-        self.h2w.plot([state.h / state.w for state in states], color=color, linewidth=1)
-        self.angle.plot([state.angle for state in states], color=color, linewidth=1)
+    def plot_states(self, states: list, timestamps=None, color = 'blue'):
+        """
+        Plot a list of states
+        :param states: The list of states to be plotted
+        :param timestamps: The timestamps of the states
+        :param color: The color used to plot the states
+        """
+        if timestamps:
+            x_axis = timestamps
+        else:
+            x_axis = range(len(states))
+        self.x.plot(x_axis, [state.x for state in states], color=color, linewidth=1)
+        self.y.plot(x_axis, [state.y for state in states], color=color, linewidth=1)
+        self.h2w.plot(x_axis, [state.h / state.w for state in states], color=color, linewidth=1)
+        self.angle.plot(x_axis, [state.angle for state in states], color=color, linewidth=1)
         plt.pause(0.001)
 
     def dismiss(self):
+        """
+        Dismiss figure
+        """
         plt.close(self.fig)
